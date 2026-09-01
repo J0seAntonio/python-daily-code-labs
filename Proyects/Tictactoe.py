0556b4adab9fd146ac7a807a815e1d7c97d4d493
J0seAntonio <delriocardonajoseantoniodev@gmail.com>
2026-09-01 13:18:53 -0500

    fix: ensure user input is converted to integer for game selection

--- Proyects/Tictactoe.py
+++ Proyects/Tictactoe.py
@@ -6,6 +6,12 @@ print("""
    ██║   ██║╚██████╗   ██║   ██║  ██║╚██████╗   ██║   ╚██████╔╝███████╗
    ╚═╝   ╚═╝ ╚═════╝   ╚═╝   ╚═╝  ╚═╝ ╚═════╝   ╚═╝    ╚═════╝ ╚══════╝
 """)
-user_answer = input(
-    "Would you like to play the game?\n1.Yes\n2. No\nSelect one option (1-2):"
+# Si no tiene la funcion int() no podra funcionar ya que mas abajo pide que sea igual a un numero pero si lo dejamos sin el int() la variable seria "1" en texto no como un numero entero.
+user_answer = int(
+    input("Would you like to play the game?\n1.Yes\n2. No\nSelect one option (1-2):")
 )
+
+if user_answer == 1:
+    print("Starting...")
+elif user_answer == 2:
+    print("Bye..")
