ee74f137fe9997bdfa6123396fb737205081a9e6
J0seAntonio <delriocardonajoseantoniodev@gmail.com>
2026-09-01 12:59:34 -0500

    add new files for basic arithmetic operations and Tictactoe game introduction (soon)

---
+++ Introduction-to-python/Day_03/numbers.py
@@ -0,0 +1,35 @@
+# You can add (+), subtract (-), multiply (*), and divide (/) integers in Python.
+addition = 2 + 3
+print(f"Thhis is a addition (2+3):{addition}")
+
+subtraction = 3 - 2
+print(f"This is a subtraction (3-2):{subtraction}")
+
+multiplication = 2 * 3
+print(f"This is a multiplication (2*3):{multiplication}")
+
+division = 3 / 2
+print(f"This is a division (3/2):{division}")
+
+# Exponents:
+exponents = 3 * 2
+print(exponents)
+
+exponents = 3**3
+print(exponents)
+
+exponents = 10**6
+print(exponents)
+
+
+# Section of try it yourself:
+
+print("\nSection of try it yourself:\n")
+
+print(5 + 3)
+print(10 - 2)
+print(4 * 2)
+print(80 / 10)
+
+favorite_number = 73
+print(f"\nMy favorite number is: {favorite_number}")
